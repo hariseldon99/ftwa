232f0a7ce4fc4b04ecf07f325444daceee33ee55
Analabha Roy <daneel@utexas.edu>
2021-09-30 16:55:14 +0530

    Started FTWA problem

---
+++ ftwa_sp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 15:03:06 2021
+# Copyright 2021 Analabha Roy (daneel@utexas.edu):
+# Released under the GPLv3 license @ https://opensource.org/licenses/GPL-3.0
+"""
